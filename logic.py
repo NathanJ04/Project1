0f3ac3a37c276fa8e1da2723060b5ca6695a2183
NathanJ04 <njohnson108@unomaha.edu>
2024-05-01 12:51:10 -0500

    Base Code Added

--- logic.py
+++ logic.py
@@ -3,6 +3,17 @@ from remotegui import *
 
 
 class Logic(QMainWindow, Ui_Dialog):
-    def __init__(self):
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
+
+    def __init__(self, status=False, muted=False, volume=MIN_VOLUME, channel=MIN_CHANNEL):
         super().__init__()
         self.setupUi(self)
+        self.__status = status
+        self.__muted = muted
+        self.__volume = volume
+        self.__channel = channel
+
+
